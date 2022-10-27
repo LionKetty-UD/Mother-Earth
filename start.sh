0c8bd1cdf7326ca9eb6bf8086da343a1f48a4d36
LionKettyUD <116444594+LionKetty-UD@users.noreply.github.com>
2022-10-27 22:10:04 +0530

    Mother-Earth

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/EvaMaria.git /EvaMaria
+  git clone https://github.com/LionKetty-UD/Mother-Earth.git /Mother-Earth
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Mother-Earth
 fi
-cd /EvaMaria
+cd /Mother-Earth
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
